464ae5349fcfff0799fd112de530211b16cffd90
Rudolf Hersen <rhersen@gmail.com>
2013-05-29 23:20:41 +0200

    happy case implementation of the proxy

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var request = require('request');
+var key = require('./key');
+
+var app = express();
+
+app.get('/departures/:id', function (req, res) {
+    request({
+            uri: 'https://api.trafiklab.se/sl/realtid/GetDpsDepartures?key=' + key.getKey() + '&timeWindow=60&siteId=' + req.params.id,
+            headers: {
+                accept: 'application/json'
+            }},
+        function (error, response, body) {
+            res.send(body);
+        });
+});
+
+app.listen(3000);
+console.log('Listening on port 3000');
